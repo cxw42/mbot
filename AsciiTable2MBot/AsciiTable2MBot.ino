a95e8ecfda8455d514620c4dbd5c5528f265bc82
Chris White <cxwembedded@gmail.com>
2023-10-15 16:28:23 -0400

    AsciiTable: add ifdef for serial

--- AsciiTable2MBot/AsciiTable2MBot.ino
+++ AsciiTable2MBot/AsciiTable2MBot.ino
@@ -19,12 +19,19 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/ASCIITable
 */
 
+// define for USB; comment out for 2.4GHz
+#define TTYUSB
+
 void setup() {
+#ifdef TTYUSB
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+#else
+  Serial.begin(57600);
+#endif
 
   // prints title with ending line break
   Serial.println("ASCII Table ~ Character Map");
