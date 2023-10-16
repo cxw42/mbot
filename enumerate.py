07ca7c9145736cb63b2abdd279c01b9740ab504e
Chris White <cxwembedded@gmail.com>
2023-10-15 22:26:56 -0400

    Experimenting with TX

--- enumerate.py
+++ enumerate.py
@@ -60,7 +60,7 @@ try:
             print("Write the data")
             # for i in range(10):
             #     h.write(bytes([0, 4, 35, 35] + [0x0d] + [0x0a]))
-            h.write(bytes([2, 0x0D, 0x0A]))
+            h.write(bytes([7]) + "Hello\n".encode('utf-8'))
 
             # wait
             time.sleep(0.05)
